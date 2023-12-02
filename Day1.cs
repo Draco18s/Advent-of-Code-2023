8c494afdc1152506644fa5b6e0da34de38e4e39b
Draco18s <draco18s@gmail.com>
2023-12-02 15:55:47 -0600

    Init

---
+++ Day1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventofCode2022 {
+	internal static class DayOne {
+		internal static long Part1(string input) {
+			return -1;
+		}
+
+		internal static long Part2(string input) {
+			return -1;
+		}
+	}
+}
