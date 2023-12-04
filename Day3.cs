d932c00f45cd1a04acd54327556ce1c4e5acc610
Draco18s <draco18s@gmail.com>
2023-12-03 23:33:17 -0600

    Days 3 and 4

---
+++ Day3.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace AdventofCode2023 {
+	internal static class Day3 {
+		internal static long Part1(string input)
+		{
+			string[] lines = input.Split('\n');
+			long result = 0l;
+			for (var l = 0; l < lines.Length; l++)
+			{
+				var line = lines[l];
+				for (var k = 0; k < line.Length; k++)
+				{
+					var c = line[k];
+					if (char.IsDigit(c))
+					{
+						int num = ExtractValue(lines, l, ref k);
+						if (num > 0)
+						{
+							result += num;
+						}
+					}
+				}
+			}
+
+			return result;
+		}
+
+		private static int ExtractValue(string[] lines, int y, ref int x)
+		{
+			char symbol = '\0';
+			for (int ox = -1; ox <= 1; ox++)
+			{
+				for (int oy = -1; oy <= 1; oy++)
+				{
+					if(ox ==0  && oy == 0) continue;
+					if(y+oy < 0) continue;
+					if(x+ox < 0) continue;
+					if(x+ox >= lines.Length) continue;
+					if(y+oy  >= lines.Length) continue;
+					if (char.IsDigit(lines[y+oy][x+ox]) || lines[y+oy][x+ox] == '.') continue;
+					symbol = lines[y+oy][x+ox];
+				}
+			}
+
+			if (symbol == '\0') return 0;
+
+			int ex = x;
+			while (x >= 0 && char.IsDigit(lines[y][x]))
+			{
+				x--;
+			}
+			while (ex < lines[y].Length && char.IsDigit(lines[y][ex]))
+			{
+				ex++;
+			}
+
+			string v = lines[y].Substring(x+1, ex-x-1);
+
+			x = ex;
+
+			return int.Parse(v);
+		}
+
+		internal static long Part2(string input) {
+			string[] lines = input.Split('\n');
+			long result = 0l;
+			for (var l = 0; l < lines.Length; l++)
+			{
+				var line = lines[l];
+				for (var k = 0; k < line.Length; k++)
+				{
+					var c = line[k];
+					if (c == '*')
+					{
+						List<int> values = new List<int>();
+						for (int oy = -1; oy <= 1; oy++)
+						{
+							for (int ox = -1; ox <= 1; ox++)
+							{
+								int X = k + ox;
+
+								if(!char.IsDigit(lines[l + oy][X])) continue;
+
+								int num = ExtractValue(lines, l + oy, ref X);
+
+								if (num > 0)
+									values.Add(num);
+								ox = X - k;
+							}
+						}
+
+						//values = values.Distinct().ToList();
+						if(values.Count != 2)
+							continue;
+						int v = values.Aggregate(1, (v, r) => r * v);
+						result += v;
+					}
+				}
+			}
+			return result;
+		}
+	}
+}
---
+++ Day4.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace AdventofCode2023 {
+	internal static class Day4 {
+		internal static long Part1(string input)
+		{
+			string[] lines = input.Split('\n');
+			long result = 0l;
+			foreach (string line in lines)
+			{
+				string[] halves = line.Split('|');
+				string[] numbers = halves[0].Split(':')[1].Split(' ');
+				string[] winning = halves[1].Split(' ');
+				
+				List<int> nums = numbers.Where(s => s.Length > 0).Select(int.Parse).ToList();
+				List<int> wins = winning.Where(s => s.Length > 0).Select(int.Parse).ToList();
+
+				int pts = nums.Aggregate(0, (r, c) =>
+				{
+					int v = wins.Contains(c) ? (r == 0 ? 1 : r * 2) : r;
+
+					return v;
+				});
+
+				result += pts;
+			}
+			return result;
+		}
+
+		internal static long Part2(string input) {
+			string[] lines = input.Split('\n');
+			long result = 0l;
+
+			Dictionary<int, int> winnings = new Dictionary<int, int>();
+			Dictionary<int, int> copies = new Dictionary<int, int>();
+			
+
+			int cardId = 0;
+			
+			foreach (string line in lines)
+			{
+				cardId++;
+				copies.Add(cardId, 1);
+				string[] halves = line.Split('|');
+				string[] numbers = halves[0].Split(':')[1].Split(' ');
+				string[] winning = halves[1].Split(' ');
+
+				List<int> nums = numbers.Where(s => s.Length > 0).Select(int.Parse).ToList();
+				List<int> wins = winning.Where(s => s.Length > 0).Select(int.Parse).ToList();
+				
+				int v = 0;
+				foreach (int i in nums)
+				{
+					if (wins.Contains(i))
+					{
+						v++;
+					}
+				}
+
+				winnings.Add(cardId, v);
+			}
+			cardId = 0;
+			foreach (string _ in lines)
+			{
+				cardId++;
+				int c = winnings[cardId];
+				for (int n = 0; n < c; n++)
+				{
+					copies[cardId+n+1] += copies[cardId];
+				}
+			}
+
+			foreach (var kvp in copies)
+			{
+				result += kvp.Value;
+			}
+			return result;
+		}
+	}
+}
