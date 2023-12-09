7a9a0c1cdf15417845538d554930ee0dc02d8a5d
Draco18s <draco18s@gmail.com>
2023-12-09 02:05:05 -0600

    Days 8 and 9

---
+++ AoCLib/AoCMath.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Draco18s.AoCLib
+{
+	public static class AoCMath
+	{
+		public static long GDC(long a, long b) => (b == 0 || b == 1) ? a : GDC(b, a % b);
+		public static long LCM(long b, long a) => (b == 0 || b == 1) ? a : a / GDC(a, b) * b;
+	}
+}
